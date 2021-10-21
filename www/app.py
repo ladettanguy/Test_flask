8894036419f16117024d9f789a4b8cd03e966488
ladettanguy <tanguy.ladet@etu.umontpellier.fr>
2021-10-21 11:35:06 +0200

    Inchallah ça marche

--- www/app.py
+++ www/app.py
@@ -4,17 +4,21 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    request.make_form_data_parser()
-    if request.is_json:
-        data = request.data
-        my_json = json.loads(data)
-        return str(my_json)
+    if request.method == 'POST':
+        request.make_form_data_parser()
+        if request.is_json:
+            data = request.data
+            my_json = json.loads(data)
+            return str(my_json)
+        else:
+            return 'Post Hello World !'
     else:
-        return 'Hello World !'
+        return 'Get Hello World !'
 
 
-@app.route('/index/')
+@app.route('/index', methods=['POST', 'GET'])
+@app.route('/index/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
